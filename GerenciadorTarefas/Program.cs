16bcd7f4f83af2be8f0bafacd5210b9df2c444eb
Carol <carolduarte2000@gmail.com>
2024-09-20 21:36:54 -0300

    nova funcionalidade: remover tarefa

--- GerenciadorTarefas/Program.cs
+++ GerenciadorTarefas/Program.cs
@@ -6,6 +6,7 @@ while (true)
     Console.WriteLine("Gerenciador de Tarefas");
     Console.WriteLine("1- Adicionar Tarefa");
     Console.WriteLine("2- Listar Tarefas");
+    Console.WriteLine("3- Remover Tarefa");
     Console.Write("Escolha uma opção: ");
     opcao = int.Parse(Console.ReadLine());
 
@@ -20,9 +21,20 @@ while (true)
         case 2:
             Console.WriteLine("Tarefas:");
             for (int i = 0; i < tarefas.Count; i++)
-                Console.WriteLine($"□ {tarefas[i]}");
+                Console.WriteLine($"{i + 1}- {tarefas[i]}");
             if (tarefas.Count == 0)
                 Console.WriteLine("Nenhuma tarefa cadastrada.");
+            break;
+        case 3:
+            Console.Write("Digite o número da tarefa que deseja remover: ");
+            int numeroTarefa = int.Parse(Console.ReadLine()) - 1;
+            if (numeroTarefa >= 0 && numeroTarefa < tarefas.Count)
+            {
+                tarefas.RemoveAt(numeroTarefa);
+                Console.WriteLine("Tarefa removida com sucesso!");
+            }
+            else
+                Console.WriteLine("Tarefa não encontrada.");
             break;
     }
 }
