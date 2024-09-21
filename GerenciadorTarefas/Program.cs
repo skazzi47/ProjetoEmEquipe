da10db78d4a76bb0f2019ec3046b1cfe50b91ad0
Carol <carolduarte2000@gmail.com>
2024-09-20 21:30:30 -0300

    adicionado menu e nova funcionalidade: listar tarefas

--- GerenciadorTarefas/Program.cs
+++ GerenciadorTarefas/Program.cs
@@ -1,9 +1,28 @@
 List<string> tarefas = new List<string>();
+int opcao;
 
-Console.WriteLine("Gerenciador de Tarefas");
+while (true)
+{
+    Console.WriteLine("Gerenciador de Tarefas");
+    Console.WriteLine("1- Adicionar Tarefa");
+    Console.WriteLine("2- Listar Tarefas");
+    Console.Write("Escolha uma opção: ");
+    opcao = int.Parse(Console.ReadLine());
 
-Console.Write("Digite a descrição da tarefa: ");
-string tarefa = Console.ReadLine();
-tarefas.Add(tarefa);
-Console.WriteLine("Tarefa adicionada com sucesso!");
-Console.WriteLine(tarefas[0]);
+    switch (opcao)
+    {
+        case 1:
+            Console.Write("Digite a descrição da tarefa: ");
+            string tarefa = Console.ReadLine();
+            tarefas.Add(tarefa);
+            Console.WriteLine("Tarefa adicionada com sucesso!");
+            break;
+        case 2:
+            Console.WriteLine("Tarefas:");
+            for (int i = 0; i < tarefas.Count; i++)
+                Console.WriteLine($"□ {tarefas[i]}");
+            if (tarefas.Count == 0)
+                Console.WriteLine("Nenhuma tarefa cadastrada.");
+            break;
+    }
+}
