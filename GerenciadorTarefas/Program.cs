2ca025f9a1855d3a956969e2c6b6d88700457848
Carol <carolduarte2000@gmail.com>
2024-09-20 21:21:29 -0300

    nova funcionalidade: adicionar tarefa

--- GerenciadorTarefas/Program.cs
+++ GerenciadorTarefas/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+List<string> tarefas = new List<string>();
+
+Console.WriteLine("Gerenciador de Tarefas");
+
+Console.Write("Digite a descrição da tarefa: ");
+string tarefa = Console.ReadLine();
+tarefas.Add(tarefa);
+Console.WriteLine("Tarefa adicionada com sucesso!");
+Console.WriteLine(tarefas[0]);
