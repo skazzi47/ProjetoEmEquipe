48b9e95d1f7b8511a0d0d4942d7fc0a9f0c62e7f
Carol <carolduarte2000@gmail.com>
2024-09-23 09:02:37 -0300

    nova funcionalidade: jogar de novo

--- 05_SimuladorLancamentoDados/Program.cs
+++ 05_SimuladorLancamentoDados/Program.cs
@@ -1,5 +1,25 @@
 ﻿Random aleatorio = new Random();
-int numeroAleatorio = aleatorio.Next(1, 7);
 
 Console.WriteLine("Simulador de Lançamento de Dados");
-Console.WriteLine($"O dado caiu em: {numeroAleatorio}");
+
+while (true)
+{
+    int numeroAleatorio = aleatorio.Next(1, 7);
+    Console.WriteLine($"O dado caiu em: {numeroAleatorio}");
+    
+    while (true)
+    {        
+        Console.Write("Jogar novamente? (S/N): ");
+        string jogarDeNovo = Console.ReadLine().ToUpper();
+
+        if (jogarDeNovo == "S")
+            break;            
+        else if (jogarDeNovo == "N")
+        {
+            Console.WriteLine("Saindo... Até a próxima!");
+            return;
+        }
+        else
+            Console.WriteLine("Opção inválida. Tente novamente.");
+    }
+}
