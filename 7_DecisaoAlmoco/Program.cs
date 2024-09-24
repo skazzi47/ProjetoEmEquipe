25064ef8dad5635576988450e36c47fe30c00498
Carol <carolduarte2000@gmail.com>
2024-09-24 19:50:28 -0300

    melhoria interface usuário e pequena correção validação menu

--- 7_DecisaoAlmoco/Program.cs
+++ 7_DecisaoAlmoco/Program.cs
@@ -1,4 +1,29 @@
-﻿List<string> restaurantes = ["Restaurante Italiano", "Churrascaria", "Restaurante Japonês", "Fast-Food", "Restaurante Vegetariano"];
+﻿void WriteTitle()
+{
+    Console.Clear();
+    Console.ForegroundColor = ConsoleColor.DarkYellow;
+    Console.WriteLine(@"   ___          _                   __      ___   __                  ");
+    Console.WriteLine(@"  / _ \___ ____(_)__ ___  ____  ___/ /__   / _ | / /_ _  ___  _______ ");
+    Console.WriteLine(@" / // / -_) __/ (_-</ _ \/ __/ / _  / -_) / __ |/ /  ' \/ _ \/ __/ _ \");
+    Console.WriteLine(@"/____/\__/\__/_/___/\___/_/    \_,_/\__/ /_/ |_/_/_/_/_/\___/\__/\___/");
+    Console.WriteLine("                                                              /_)      ");
+    Console.ResetColor();
+}
+void WriteSubtitle(string texto)
+{
+    Console.WriteLine();
+    Console.ForegroundColor = ConsoleColor.DarkYellow;
+    Console.WriteLine(texto);
+    Console.ResetColor();
+}
+void ColorLineRed(string texto)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(texto);
+    Console.ResetColor();
+}
+
+List<string> restaurantes = ["Restaurante Italiano", "Churrascaria", "Restaurante Japonês", "Fast-Food", "Restaurante Vegetariano"];
 List<List<string>> pratosPorRestaurante = [
     [ "Lasanha de Berinjela", "Pizza Margherita", "Torta de Ricota" ],       // Italiano
     [ "Batata Frita", "Picanha na Brasa", "Pizza Margherita" ],              // Churrascaria
@@ -7,27 +32,26 @@ List<List<string>> pratosPorRestaurante = [
     [ "Lasanha de Berinjela", "Salada Caesar Vegana", "Tempura de Legumes" ] // Vegetariano
 ];
 Random aleatorio = new Random();
-int numMenu = 0;
-int numRestaurante = 0;
-int numPrato = 0;
+int numMenu = 0, numRestaurante = 0, numPrato = 0;
 
-Console.WriteLine("Decisor de Almoço");
+WriteTitle();
 Console.WriteLine("1- Escolher restaurante");
 Console.WriteLine("2- Escolher prato");
 Console.WriteLine("3- Estou indeciso(a), escolha por mim");
 while (true)
 {
     Console.Write("Digite sua opção: ");
-    if (int.TryParse(Console.ReadLine(), out numMenu) && numMenu == 1 || numMenu == 2)
+    if (int.TryParse(Console.ReadLine(), out numMenu) && numMenu == 1 || numMenu == 2 || numMenu == 3)
         break;
     else
-        Console.WriteLine("Opção inválida. Tente novamente.");
+        ColorLineRed("Opção inválida. Tente novamente.");
 }
 
 switch (numMenu)
 {
     case 1:
-        Console.WriteLine("Opções de Restaurantes");
+        WriteTitle();
+        WriteSubtitle("Opções de Restaurantes");
         for (int i = 0; i < restaurantes.Count(); i++)
             Console.WriteLine($"{i + 1}- {restaurantes[i]}");
 
@@ -40,9 +64,10 @@ switch (numMenu)
                 break;
             }
             else
-                Console.WriteLine("Opção inválida. Tente novamente.");
+                ColorLineRed("Opção inválida. Tente novamente.");
         }
-        Console.WriteLine($"Opções de Pratos em {restaurantes[numRestaurante]}");
+        WriteTitle();
+        WriteSubtitle($"Opções de Pratos em {restaurantes[numRestaurante]}");
         for (int i = 0; i < pratosPorRestaurante[numRestaurante].Count; i++)
             Console.WriteLine($"{i + 1}- {pratosPorRestaurante[numRestaurante][i]}");
 
@@ -55,13 +80,14 @@ switch (numMenu)
                 break;
             }
             else
-                Console.WriteLine("Opção inválida. Tente novamente.");
+                ColorLineRed("Opção inválida. Tente novamente.");
         }
-
+        WriteTitle();
         Console.WriteLine($"Você escolheu almoçar {pratosPorRestaurante[numRestaurante][numPrato]} em {restaurantes[numRestaurante]}. Uma ótima pedida!");
         break;
 
     case 2:
+        WriteTitle();
         HashSet<string> pratosUnicos = [];
 
         foreach (var pratos in pratosPorRestaurante)
@@ -69,7 +95,7 @@ switch (numMenu)
 
         List<string> listaPratos = pratosUnicos.ToList();
 
-        Console.WriteLine("Opções de Pratos");
+        WriteSubtitle("Opções de Pratos");
         for (int i = 0; i < listaPratos.Count; i++)
             Console.WriteLine($"{i + 1}- {listaPratos[i]}");
 
@@ -82,7 +108,7 @@ switch (numMenu)
                 break;
             }
             else
-                Console.WriteLine("Opção inválida. Tente novamente.");
+                ColorLineRed("Opção inválida. Tente novamente.");
         }
 
         List<string> restaurantesComPrato = [];
@@ -94,7 +120,7 @@ switch (numMenu)
         }
         if (restaurantesComPrato.Count > 0)
         {
-            Console.WriteLine($"Restaurantes que oferecem {listaPratos[numPrato]}");
+            WriteSubtitle($"Restaurantes que oferecem {listaPratos[numPrato]}");
 
             for (int i = 0; i < restaurantesComPrato.Count; i++)
                 Console.WriteLine($"{i + 1}- {restaurantesComPrato[i]}");
@@ -108,16 +134,22 @@ switch (numMenu)
                     break;
                 }
                 else
-                    Console.WriteLine("Opção inválida. Tente novamente.");
+                    ColorLineRed("Opção inválida. Tente novamente.");
             }
-
+            WriteTitle();
             Console.WriteLine($"Você escolheu almoçar {listaPratos[numPrato]} em {restaurantesComPrato[numRestaurante]}. Uma ótima pedida!");
         }
         break;
 
     case 3:
+        WriteTitle();
+        WriteSubtitle("3 - Estou indeciso(a), escolha por mim");
         int restauranteAleatorio = aleatorio.Next(restaurantes.Count);
         int pratoAleatorio = aleatorio.Next(pratosPorRestaurante[restauranteAleatorio].Count);
-        Console.WriteLine($"Que tal almoçar {pratosPorRestaurante[restauranteAleatorio][pratoAleatorio]} em {restaurantes[restauranteAleatorio]}?");
+        Console.WriteLine($"Sugiro que almoce {pratosPorRestaurante[restauranteAleatorio][pratoAleatorio]} em {restaurantes[restauranteAleatorio]}. Que tal?");
+        break;
+
+    default:
+        ColorLineRed("Opção inválida. Tente novamente.");
         break;
 }
